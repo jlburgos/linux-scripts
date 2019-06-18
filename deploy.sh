9dc140b3c135160c4596d262f1eeb52e8b874c68
root <root@jlburgos-icp1.fyre.ibm.com>
2019-06-18 07:36:06 -0700

    Unnecessary file check.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[[ ! -f "$HOME/.vimrc" ]] && ln -s $(pwd)/vimrc $HOME/.vimrc
-[[ ! -f "$HOME/.screenrc" ]] && ln -s $(pwd)/screenrc $HOME/.screenrc
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/screenrc $HOME/.screenrc
